147d6ff8b9974433d3967bae542d84ac3876aef6
Ejike Obineme <eobineme@gmail.com>
2019-09-03 13:52:32 -0500

    use form in all views, back to working state

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -17,7 +17,7 @@ class NewVisitorTest(FunctionalTest):
         
 
         # She is invited to enter a to-do item straight away.
-        input = self.get_item_input_box()
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
